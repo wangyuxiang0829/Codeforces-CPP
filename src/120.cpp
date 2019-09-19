68c8faf74fc83123743e60f527e473f815e2a6fb
wangyuxiang <yuxiangwang0829@gmail.com>
2019-09-19 21:56:12 +0800

    update 120

--- src/120.cpp
+++ src/120.cpp
@@ -1,30 +1,16 @@
 #include <vector>
 #include <algorithm>
-#include <utility>
 #include <cassert>
 
 class Solution {
 public:
     int minimumTotal(std::vector<std::vector<int>> &triangle) {
-        int n = triangle.size();
-        if (n == 0)
-            return 0;
-        int dp[n][n] = {};
-        dp[0][0] = triangle[0][0];
-        for (int i = 1; i < n; ++i) {
-            int length = triangle[i].size();
-            dp[i][0] = dp[i - 1][0] + triangle[i][0];
-            dp[i][length - 1] = dp[i - 1][length - 2] + triangle[i][length - 1];
-            for (int j = 1; j < length - 1; ++j) {
-                dp[i][j] = std::min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+        for (int i = triangle.size() - 2; i >= 0; --i) {
+            for (int j = 0; j < triangle[i].size(); ++j) {
+                triangle[i][j] = std::min(triangle[i + 1][j], triangle[i + 1][j + 1]) + triangle[i][j];
             }
         }
-        int min = dp[n - 1][0];
-        for (int j = 1; j < n; ++j) {
-            if (dp[n - 1][j] < min)
-                min = dp[n - 1][j];
-        }
-        return min;
+        return triangle[0][0];
     }
 };
 
