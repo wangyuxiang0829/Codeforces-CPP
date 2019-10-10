4917b98e1f786a96f66d94ffe8b1702bc1e3f3d6
Yuxiang Wang <40556504+wangyuxiang0829@users.noreply.github.com>
2019-10-10 21:08:28 +0800

    update minimum window substring

--- src/double_pointer/76.cpp
+++ src/double_pointer/76.cpp
@@ -6,18 +6,17 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         int match = 0, min = 0, start = 0, m = s.size(), n = t.size();
-        unordered_map<char, int> map;
         unordered_map<char, int> count;
         for (auto c : t)
-            map[c]++;
+            count[c]++;
         for (int i = 0, j = 0; j < m; ++j) {
-            if (map.find(s[j]) != map.end() && ++count[s[j]] == map[s[j]]) match++;
-            while (match == map.size()) {
+            if (count.find(s[j]) != count.end() && count[s[j]]-- > 0) match++;
+            while (match == t.size()) {
                 if (j - i + 1 < min || min == 0) {
                     min = j - i + 1;
                     start = i;
                 }
-                if (map.find(s[i]) != map.end() && count[s[i]]-- == map[s[i]]) match--;
+                if (count.find(s[i]) != count.end() && ++count[s[i]] > 0) match--;
                 i++;
             }
         }
