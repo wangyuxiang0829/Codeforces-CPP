259aa269663febc7561767f467d30b9f643583a9
Yuxiang Wang <40556504+wangyuxiang0829@users.noreply.github.com>
2019-10-11 22:57:19 +0800

    lru cache

---
+++ src/design/146.cpp
@@ -0,0 +1,50 @@
+#include <list>
+#include <utility>
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+class LRUCache {
+public:
+    LRUCache(int capacity) : cap(capacity) {
+    }
+
+    int get(int key) {
+        if (map.find(key) == map.end()) return -1;
+        auto key_value = *map[key];
+        cache.erase(map[key]);
+        cache.push_front(key_value);
+        map[key] = cache.begin();
+        return key_value.second;
+    }
+
+    void put(int key, int value) {
+        if (map.find(key) == map.end()) {
+            if (cache.size() == cap) {
+                map.erase(cache.back().first);
+                cache.pop_back();
+            }
+        }
+        else {
+            cache.erase(map[key]);
+        }
+        cache.push_front({key, value});
+        map[key] = cache.begin();
+    }
+private:
+    int cap;
+    list<pair<int, int>> cache;
+    unordered_map<int, list<pair<int, int>>::iterator> map;
+};
+
+int main() {
+    LRUCache *obj = new LRUCache(2);
+    obj->put(1, 1);
+    obj->put(2, 2);
+    cout << obj->get(1) << endl;
+    obj->put(3, 3);
+    cout << obj->get(2) << endl;
+    obj->put(4, 4);
+    cout << obj->get(1) << endl;
+    cout << obj->get(3) << endl;
+    cout << obj->get(4) << endl;
+}
