1fe53c63d69c847167dd35293c29c928b7e7bec4
Yuxiang Wang <40556504+wangyuxiang0829@users.noreply.github.com>
2019-10-15 19:25:29 +0800

    maximum equal frequency

---
+++ src/weekly_contest/158/5225.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    int maxEqualFreq(vector<int> &nums) {
+        unordered_map<int, int> count;
+        unordered_map<int, int> frequ;
+        int ans = 0;
+        for (auto iter = nums.begin(); iter != nums.end(); ++iter) {
+            auto times = ++count[*iter];
+            if (times != 1)
+                if (--frequ[times - 1] == 0)
+                    frequ.erase(times - 1);
+            frequ[times]++;
+            if (frequ.size() == 1) {
+                auto p = frequ.begin();
+                if (p->first == 1 || p->second == 1)
+                    ans = max(ans, p->first * p->second);
+            }
+            else if (frequ.size() == 2) {
+                auto p = frequ.begin(), q = p++;
+                if (p->first > q->first) swap(p, q);
+                if ((p->second == 1 && p->first == 1) || (q->second == 1 && q->first == p->first + 1))
+                    ans = max(ans, p->first * p->second + q->first * q->second);
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 2, 1, 1, 5, 3, 3, 5};
+    cout << Solution().maxEqualFreq(nums) << endl;
+}
