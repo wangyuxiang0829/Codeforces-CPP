e8eac9d0176648a032bcba9e535edeedeaa012b9
Yuxiang Wang <40556504+wangyuxiang0829@users.noreply.github.com>
2019-10-15 21:07:58 +0800

    Update 706.cpp

--- src/design/706.cpp
+++ src/design/706.cpp
@@ -2,8 +2,10 @@
 #include <list>
 #include <vector>
 #include <iostream>
+#include <functional>
 #include <algorithm>
 using namespace std;
+using namespace placeholders;
 class MyHashMap {
 public:
     MyHashMap() : table(N) {
@@ -11,32 +13,30 @@ public:
 
     void put(int key, int value) {
         auto &bucket = table[key % N];
-        auto iter = find_if(bucket.begin(), bucket.end(), [=](pair<int, int> p) {
-                return p.first == key;
-                });
+        auto iter = find_if(bucket.begin(), bucket.end(), bind(equal, _1, key));
         if (iter == bucket.end()) bucket.emplace_back(key, value);
         else iter->second = value;
     }
 
     int get(int key) {
         auto &bucket = table[key % N];
-        auto iter = find_if(bucket.begin(), bucket.end(), [=](pair<int, int> p) {
-                return p.first == key;
-                });
+        auto iter = find_if(bucket.begin(), bucket.end(), bind(equal, _1, key));
         if (iter == bucket.end()) return -1;
         else return iter->second;
     }
 
     void remove(int key) {
         auto &bucket = table[key % N];
-        auto iter = find_if(bucket.begin(), bucket.end(), [=](pair<int, int> p) {
-                return p.first == key;
-                });
+        auto iter = find_if(bucket.begin(), bucket.end(), bind(equal, _1, key));
         if (iter != bucket.end()) bucket.erase(iter);
     }
 private:
-    constexpr static int N = 1e5;
+    constexpr static int N = 1e5 + 7;
     vector<list<pair<int, int>>> table;
+
+    static bool equal(pair<int, int> p, int key) {
+        return key == p.first;
+    }
 };
 
 int main() {
