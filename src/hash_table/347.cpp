d865c97a1208e4cc39dffe698b2ac2500de1c0aa
Yuxiang Wang <40556504+wangyuxiang0829@users.noreply.github.com>
2019-10-16 18:19:09 +0800

    Update 347.cpp

--- src/hash_table/347.cpp
+++ src/hash_table/347.cpp
@@ -11,19 +11,18 @@ public:
             count[num]++;
         using value_t = pair<int, int>;
         vector<value_t> container(count.begin(), count.end());
-        sort(container.begin(), container.end(), [](value_t v1, value_t v2) {
-                return v1.second > v2.second;});
-        auto iter = container.begin();
+        auto comp = [](const value_t &v1, const value_t &v2) {return v1.second < v2.second;};
+        make_heap(container.begin(), container.end(), comp);
         vector<int> result;
-        while (k--) {
-            result.push_back(iter->first);
-            iter++;
+        for (int i = 0; i < k; ++i) {
+            result.push_back(container.begin()->first);
+            pop_heap(container.begin(), container.end() - i, comp);
         }
         return result;
     }
 };
 
 int main() {
-    for (auto i : Solution().topKFrequent({1, 1, 1, 2, 2, 3}, 2))
+    for (auto i : Solution().topKFrequent({5, 3, 1, 1, 1, 3, 5, 73, 1}, 3))
         cout << i << endl;
 }
