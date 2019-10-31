ec51ac90052fc73650ef72b5e4c337c692f06aa5
Yuxiang Wang <40556504+wangyuxiang0829@users.noreply.github.com>
2019-10-31 17:45:37 +0800

    Update 349.cpp

--- src/hash_table/349.cpp
+++ src/hash_table/349.cpp
@@ -7,6 +7,7 @@ public:
     vector<int> intersection(vector<int> &nums1, vector<int> &nums2) {
         unordered_set<int> s(nums1.begin(), nums1.end());
         unordered_set<int> t(nums2.begin(), nums2.end());
+        if (s.size() < t.size()) s.swap(t);
         vector<int> result;
         for (auto i : t) {
             if (s.find(i) != s.end())
