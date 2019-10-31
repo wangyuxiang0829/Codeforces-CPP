2eee13cef41aee8032310e81e119b22eef27e228
Yuxiang Wang <40556504+wangyuxiang0829@users.noreply.github.com>
2019-10-31 17:28:32 +0800

    intersection of two arrays

---
+++ src/hash_table/349.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+class Solution {
+public:
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2) {
+        unordered_set<int> s(nums1.begin(), nums1.end());
+        unordered_set<int> t(nums2.begin(), nums2.end());
+        vector<int> result;
+        for (auto i : t) {
+            if (s.find(i) != s.end())
+                result.push_back(i);
+        }
+        return result;
+    }
+};
+
+int main() {
+    vector<int> nums1 = {1, 2, 2, 1};
+    vector<int> nums2 = {2, 2};
+    for (auto i : Solution().intersection(nums1, nums2))
+        cout << i << endl;
+}
